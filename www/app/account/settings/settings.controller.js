ff8b5bf883b0c516c0ff7042c8a135149876d84e
Andy Smircich <andy@smokeybayair.com>
2017-04-01 15:06:53 -1100

    add email change function in settings

--- www/app/account/settings/settings.controller.js
+++ www/app/account/settings/settings.controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('workspaceApp')
-  .controller('SettingsController', function (Auth, $state) {
-  //start-non-standard
+  .controller('SettingsController', function (Auth, $state,Modal) {
+  
     this.errors = {};
     this.submitted = false;
+    this.quickMessage = Modal.confirm.quickMessage();
 
-
-              var self = this;
+  var self = this;
 
   this.changePassword = function(form) {
     this.submitted = true;
@@ -24,5 +24,21 @@ angular.module('workspaceApp')
         });
     }
   };
+  
+  this.changeEmail = function(form) {
+    this.submitted = true;
+
+    if (form.$valid) {
+      this.Auth.changeEmail(this.user.newEmail)
+        .then(function() {
+          self.emailMessage = 'Email successfully changed.';
+        })
+        .catch(function(err) {
+          form.newEmail.$setValidity('mongoose', false);
+          self.quickMessage(err.data.errors[0].message);
+          self.emailMessage = '';
+        });
+    }
+  }
   
 });
